12c96fb3c7cd9c184b4af50a65ff062ea39d45de
StepanovVasily <01vasil@mail.ru>
2016-02-28 20:18:42 +0300

    Переделка сервера

--- Main.js
+++ Main.js
@@ -7,17 +7,6 @@
 app.get('/', function(req, res){
  
 
-
-//if(req.url=="/favicon.ico")//отправка иконки при обращении клиента
-//res.end(fs.readFileSync('favicon.ico'));
-
-//if(req.url=="resourse/info")//загрузка файла "info" при маршруте "resourse/info"
-//var text = fs.readFileSync('resourse/info.html', 'utf8');
-
-//else
-//var text = fs.readFileSync('index', 'utf8');//В остальных случаях загружать файл "index"
-
-//res.end(text);	
 });
 
 app.listen(2000);
--- routes/users.js
+++ routes/users.js
@@ -3,55 +3,52 @@ var path = require('path');
 var fs = require('fs')
 var util = require('util')
 var router = express.Router();
+var url = require('url');
 var nedb = require('nedb')
   , planets = new nedb({ filename: path.join(__dirname, '../Data', 'DataB.db'), autoload: true });
-
 
 router.get('/', function(req, res, next) {
 // Let's insert some data
 var app = express();
-
      
-      // Now we can query it the usual way
-     // planets.find({ satellites: { $lt: 10 } }, function (err, docs) {
-        // docs is an array containing Earth and Mars
-     
- //planets.find({satellites:{$eq:1}},function(err,docs){res.write('тут'')});
+ //Парсинг запросов
+var i=Object.keys( req.query);
+var planetName=req.query[i[0]];
+var countSat=req.query[i[1]];
 
-/* GET users listing. */
+//управление бд
+if(req.query.Изменить=="Изменить")
+planets.update({'_id':i[0]},{$set: { name:planetName,satellites:countSat}}, true);
+if(req.query.Удалить=="Удалить")
+planets.remove({ _id:i[0] }, {});
+if(req.query.Добавить=="Добавить")
+planets.insert({ name: '', satellites: 0 });
 
 
 res.write('	<!DOCTYPE html><html>\
-<head>\
-        <meta charset="utf-8">\
+<head>  <meta charset="utf-8">\
         <title>База данных</title>\
         <link rel="stylesheet" href="stylesheets/style.css">\
     </head>\
     <body>\
     <a href="/"><-Меню</a>\
-        <h1>Информация</h1>\
-        <p>Это страница с информацией</p>\
-        <p>Тут пока пусто</p>');
-//res.write(req.query);
-planets.update(req.query._id,req.query.name);
+        <h1>Планеты</h1>\
+        <p>Список планет и количество их спутников</p>');
+//генерация таблицы
        planets.find({}).exec(function (err,docs)
        {
  res.write('<table cellpadding="3"  >');      	
       for(var i=0; i<docs.length; i++)
-  
 res.write(' <td><form><input name="'+docs[i]._id+'" size ="10" value="'+docs[i].name+'" <td>\
-	<input type="submit" value="Изменить">\
-	<input type="submit" value="Удaлить">\
-	<td><td><td><input name="name" size ="10" value="'+
+	<input name="satellites" size ="10" value="'+
 	docs[i].satellites+'"<td>\
-	<input type="submit" value="Изменить">\
-	<input type="submit" value="Удалить"></form><tr>');
-res.write('</body></html>');
-
-
-
+	<input type="submit" value="Изменить" name="Изменить">\
+	<input type="submit" value="Удалить" name="Удалить"></form><tr>');
+res.write('\
+<form><input type="submit" value="Добавить" name="Добавить">\
+</form></body></html>\
+	');
  res.end('');
        });
 });
-
 module.exports = router;
